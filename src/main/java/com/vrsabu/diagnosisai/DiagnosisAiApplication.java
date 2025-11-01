15f8c08b8e45906f6cf2d83cb35f41e03e33bfc4
Vyankatesh Sabu <vrsabu545@gmail.com>
2025-11-01 11:37:29 +0530

    Initial commit

---
+++ src/main/java/com/vrsabu/diagnosisai/DiagnosisAiApplication.java
@@ -0,0 +1,13 @@
+package com.vrsabu.diagnosisai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrsabu/diagnosisai/DiagnosisAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrsabu.diagnosisai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
